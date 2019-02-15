a668113f0a284572cfd5ef412d4f224205061b91
Datdt <dat-dt@trente.asia>
2019-02-15 09:30:33 +0700

    fix UI

--- js/test.js
+++ js/test.js
@@ -1,12 +1,12 @@
 function test(){
-	var tiendat = 23;
+ var tiendat = 23;
 	var result = "";
 	if(tiendat > 23){
 		result = "tien";
 	} else {
 		result = "dat";
 	}
-  return result;	
+  return result + 1;	
 	                              //djfjdsfh
 	                              //abcdef
 }
